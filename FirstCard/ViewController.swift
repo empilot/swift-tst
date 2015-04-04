76ec7af0853cef5d5bd4110fe482d1b890a2eee2
50ohm <shane.d.shu@gmail.com>
2015-04-05 00:07:55 +0800

    Initial Commit

---
+++ FirstCard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstCard
+//
+//  Created by Shane on 15/4/5.
+//  Copyright (c) 2015年 Shane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
